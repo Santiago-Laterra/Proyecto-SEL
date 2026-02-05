0f4beec4be12cc1e4d9e55280e321b140295e078
Santiago Laterra <santiagolaterra05@gmail.com>
2026-02-05 14:38:18 -0300

    add: tailwind al proyecto, creacion de componente Home y se visualiza correctamente

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-900">
+      <div className="">
+        <h1 className="text-5xl font-black">
+          SeloYah Online 🚀
+        </h1>
+        <p className="text-gray-400 mt-4 text-center">Tailwind está activo y listo para el Admin.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
