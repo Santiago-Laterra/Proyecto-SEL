d22890a4b00b9b16f779ecd9b206f5fe216215d0
Santiago Laterra <santiagolaterra05@gmail.com>
2026-02-13 11:11:14 -0300

    cambios para ver si funciona el mail

--- backend/src/services/emailServices.ts
+++ backend/src/services/emailServices.ts
@@ -4,9 +4,6 @@ import nodemailer from 'nodemailer';
 // En emailServices.ts
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  host: process.env.EMAIL_HOST,
-  port: 465,
-  secure: true, // FALSE para puerto 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
