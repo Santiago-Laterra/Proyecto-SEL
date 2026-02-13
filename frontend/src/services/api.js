1701ec2f1e829aa487a4fc6093f3621ffd98bd28
Santiago Laterra <santiagolaterra05@gmail.com>
2026-02-12 21:56:19 -0300

    fix:deployamiento final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Este interceptor es el que "habla" con el middleware del backend
